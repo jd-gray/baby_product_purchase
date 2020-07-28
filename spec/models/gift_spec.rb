8bb081cb405bc2ae23d98fd83252cd01d15c314c
Jared Gray <jared@jaredgray.us>
2020-07-27 18:42:05 -0700

    Write happy and sad path for feature spec when purchasing a product
    Add model test for gift spec

--- spec/models/gift_spec.rb
+++ spec/models/gift_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Gift, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+    describe "#validations" do
+    it "requires from" do
+      gift = Gift.new(
+        message: "Some message",
+      )
+
+      expect(gift.valid?).to eq(false)
+      expect(gift.errors[:from].size).to eq(1)
+    end
+  end
 end
